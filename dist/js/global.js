3843008b3c729651f6c3ea6bbfb69ecc063ee079
George Brook <georgebrook@gmail.com>
2018-07-09 09:34:58 +0100

    :thought_balloon:

--- dist/js/global.js
+++ dist/js/global.js
@@ -1,5 +1,16 @@
+'use strict';
 
 // ES6 Supported.
 // Go!
-"use strict";
+
+var count = function count(string) {
+  var arr = string.split('').join(''),
+      charInst = 0,
+      instArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    instArr.push(arr[i]);
+  }
+  return instArr;
+};
+console.log(count('ababccff'));
 //# sourceMappingURL=global.js.map
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,14 @@
 
 // ES6 Supported.
 // Go!
+
+const count = (string) => {
+  var arr = string.split('').join(''),
+      charInst = 0,
+      instArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    instArr.push(arr[i]);
+  }
+  return instArr;
+};
+console.log(count('ababccff'));
